25f46912ff099c4b343e41845ef82ab06ba48e56
elbedewi95 <vickybedewi-95@hotmail.com>
2022-11-14 09:07:38 -0800

    about

---
+++ src/pages/aboutMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AboutPage (){
+    return (
+        <section className="left">
+            <h1 className="H1">Welcome to my Portfolio, my name is Victoria, but you can call me Vicky!</h1>
+            <p>I am aspiring for a career in Web Development and related fields, with intuitive problem-solving skills. Proficient in both front-end and back-end technologies: HTML, CSS, JavaScript, jQuery, Node.js, React.js, MySQL, MongoDB, and Frameworks such as Bootstrap and Materialize CSS, as well as a strong understanding and knowledge of algorithms and data structures, implemented in JavaScript, Java, C++, C#, and Python.</p>
+            
+        </section>
+    )
+};
+
+export default AboutPage;
